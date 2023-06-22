53e7ab286433e78f2d649b5155b9e702d4825290
Tobias Hunger <tobias.hunger@slint-ui.com>
2023-06-22 18:57:35 +0200

    Winit update to master

--- internal/common/key_codes.rs
+++ internal/common/key_codes.rs
@@ -25,32 +25,32 @@
 macro_rules! for_each_special_keys {
     ($macro:ident) => {
         $macro![
-'\u{0008}'  # Backspace   # Qt_Key_Key_Backspace    # Back         # KEY_BackSpace  ;
-'\u{0009}'  # Tab         # Qt_Key_Key_Tab          # Tab          # KEY_Tab        ;
-'\u{000a}'  # Return      # Qt_Key_Key_Enter|Qt_Key_Key_Return # NumpadEnter|Return # KEY_Return;
+'\u{0008}'  # Backspace   # Qt_Key_Key_Backspace    # Backspace     # KEY_BackSpace  ;
+'\u{0009}'  # Tab         # Qt_Key_Key_Tab          # Tab           # KEY_Tab        ;
+'\u{000a}'  # Return      # Qt_Key_Key_Enter|Qt_Key_Key_Return # NumpadEnter|Enter # KEY_Return;
 '\u{001b}'  # Escape      # Qt_Key_Key_Escape       # Escape       # KEY_Escape     ;
 '\u{0019}'  # Backtab     # Qt_Key_Key_Backtab      #              # KEY_BackTab    ;
 '\u{007f}'  # Delete      # Qt_Key_Key_Delete       # Delete       # KEY_Delete     ;
 
 // The modifier key codes comes from https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/keyCode.
-'\u{0010}'  # Shift       # Qt_Key_Key_Shift        # LShift       # KEY_Shift_L    ;
-'\u{0011}'  # Control     # Qt_Key_Key_Control      # LControl     # KEY_Control_L  ;
-'\u{0012}'  # Alt         # Qt_Key_Key_Alt          # LAlt         # KEY_Alt_L      ;
-'\u{0013}'  # AltGr       # Qt_Key_Key_AltGr        # RAlt         # KEY_Mode_switch;
+'\u{0010}'  # Shift       # Qt_Key_Key_Shift        # ShiftLeft    # KEY_Shift_L    ;
+'\u{0011}'  # Control     # Qt_Key_Key_Control      # ControlLeft  # KEY_Control_L  ;
+'\u{0012}'  # Alt         # Qt_Key_Key_Alt          # AltLeft      # KEY_Alt_L      ;
+'\u{0013}'  # AltGr       # Qt_Key_Key_AltGr        # AltRight     # KEY_Mode_switch;
 '\u{0014}'  # CapsLock    # Qt_Key_Key_CapsLock     #              # KEY_Caps_Lock  ;
 
-'\u{0015}'  # ShiftR      #                         # RShift       # KEY_Shift_R    ;
-'\u{0016}'  # ControlR    #                         # RControl     # KEY_Control_R  ;
+'\u{0015}'  # ShiftR      #                         # ShiftRight   # KEY_Shift_R    ;
+'\u{0016}'  # ControlR    #                         # ControlRight # KEY_Control_R  ;
 
 // Use custom codes instead of DOM_VK_META for meta, because the Mozilla defined code is a regular character (E0; LATIN SMALL LETTER A WITH GRAVE)
 // which makes those keys appear as text.
-'\u{0017}'  # Meta        # Qt_Key_Key_Meta         # LWin         # KEY_Meta_L     ;
-'\u{0018}'  # MetaR       #                         # RWin         # KEY_Meta_R     ;
+'\u{0017}'  # Meta        # Qt_Key_Key_Meta         # SuperLeft    # KEY_Meta_L     ;
+'\u{0018}'  # MetaR       #                         # SuperRight   # KEY_Meta_R     ;
 
-'\u{F700}'	# UpArrow     # Qt_Key_Key_Up           # Up           # KEY_Up         ;
-'\u{F701}'	# DownArrow   # Qt_Key_Key_Down         # Down         # KEY_Down       ;
-'\u{F702}'	# LeftArrow   # Qt_Key_Key_Left         # Left         # KEY_Left       ;
-'\u{F703}'	# RightArrow  # Qt_Key_Key_Right        # Right        # KEY_Right      ;
+'\u{F700}'	# UpArrow     # Qt_Key_Key_Up           # ArrowUp      # KEY_Up         ;
+'\u{F701}'	# DownArrow   # Qt_Key_Key_Down         # ArrowDown    # KEY_Down       ;
+'\u{F702}'	# LeftArrow   # Qt_Key_Key_Left         # ArrowLeft    # KEY_Left       ;
+'\u{F703}'	# RightArrow  # Qt_Key_Key_Right        # ArrowRight   # KEY_Right      ;
 '\u{F704}'	# F1          # Qt_Key_Key_F1           # F1           # KEY_F1         ;
 '\u{F705}'	# F2          # Qt_Key_Key_F2           # F2           # KEY_F2         ;
 '\u{F706}'	# F3          # Qt_Key_Key_F3           # F3           # KEY_F3         ;
@@ -75,32 +75,32 @@ macro_rules! for_each_special_keys {
 '\u{F719}'	# F22         # Qt_Key_Key_F22          # F22          # KEY_F22        ;
 '\u{F71A}'	# F23         # Qt_Key_Key_F23          # F23          # KEY_F23        ;
 '\u{F71B}'	# F24         # Qt_Key_Key_F24          # F24          # KEY_F24        ;
-//'\u{F71C}'# F25         # Qt_Key_Key_F25          #              # KEY_F25        ;
-//'\u{F71D}'# F26         # Qt_Key_Key_F26          #              # KEY_F26        ;
-//'\u{F71E}'# F27         # Qt_Key_Key_F27          #              # KEY_F27        ;
-//'\u{F71F}'# F28         # Qt_Key_Key_F28          #              # KEY_F28        ;
-//'\u{F720}'# F29         # Qt_Key_Key_F29          #              # KEY_F29        ;
-//'\u{F721}'# F30         # Qt_Key_Key_F30          #              # KEY_F30        ;
-//'\u{F722}'# F31         # Qt_Key_Key_F31          #              # KEY_F31        ;
-//'\u{F723}'# F32         # Qt_Key_Key_F32          #              # KEY_F32        ;
-//'\u{F724}'# F33         # Qt_Key_Key_F33          #              # KEY_F33        ;
-//'\u{F725}'# F34         # Qt_Key_Key_F34          #              # KEY_F34        ;
-//'\u{F726}'# F35         # Qt_Key_Key_F35          #              # KEY_F35        ;
+//'\u{F71C}'# F25         # Qt_Key_Key_F25          # F25          # KEY_F25        ;
+//'\u{F71D}'# F26         # Qt_Key_Key_F26          # F26          # KEY_F26        ;
+//'\u{F71E}'# F27         # Qt_Key_Key_F27          # F27          # KEY_F27        ;
+//'\u{F71F}'# F28         # Qt_Key_Key_F28          # F28          # KEY_F28        ;
+//'\u{F720}'# F29         # Qt_Key_Key_F29          # F29          # KEY_F29        ;
+//'\u{F721}'# F30         # Qt_Key_Key_F30          # F30          # KEY_F30        ;
+//'\u{F722}'# F31         # Qt_Key_Key_F31          # F31          # KEY_F31        ;
+//'\u{F723}'# F32         # Qt_Key_Key_F32          # F32          # KEY_F32        ;
+//'\u{F724}'# F33         # Qt_Key_Key_F33          # F33          # KEY_F33        ;
+//'\u{F725}'# F34         # Qt_Key_Key_F34          # F34          # KEY_F34        ;
+//'\u{F726}'# F35         # Qt_Key_Key_F35          # F35          # KEY_F35        ;
 '\u{F727}'	# Insert      # Qt_Key_Key_Insert       # Insert       # KEY_Insert     ;
-//'\u{F728}'	# Delete     ;  // already as a control code
+//'\u{F728}'	# Delete;  // already as a control code
 '\u{F729}'	# Home        # Qt_Key_Key_Home         # Home         # KEY_Home       ;
 //'\u{F72A}'	# Begin       #                         #              ;
 '\u{F72B}'	# End         # Qt_Key_Key_End          # End          # KEY_End        ;
 '\u{F72C}'	# PageUp      # Qt_Key_Key_PageUp       # PageUp       # KEY_Page_Up    ;
 '\u{F72D}'	# PageDown    # Qt_Key_Key_PageDown     # PageDown     # KEY_Page_Down  ;
 //'\u{F72E}'	# PrintScreen #                         # Snapshot     ;
-'\u{F72F}'	# ScrollLock  # Qt_Key_Key_ScrollLock   # Scroll       # KEY_Scroll_Lock;
+'\u{F72F}'	# ScrollLock  # Qt_Key_Key_ScrollLock   # ScrollLock   # KEY_Scroll_Lock;
 '\u{F730}'	# Pause       # Qt_Key_Key_Pause        # Pause        # KEY_Pause      ;
-'\u{F731}'	# SysReq      # Qt_Key_Key_SysReq       # Sysrq        # KEY_Sys_Req    ;
+'\u{F731}'	# SysReq      # Qt_Key_Key_SysReq       # PrintScreen  # KEY_Sys_Req    ;
 //'\u{F732}'	# Break       #                         #              ;
 //'\u{F733}'	# Reset       #                         #              ;
-'\u{F734}'	# Stop        # Qt_Key_Key_Stop         # Stop         # KEY_XF86Stop       ;
-'\u{F735}'	# Menu        # Qt_Key_Key_Menu         #              # KEY_Menu       ;
+'\u{F734}'	# Stop        # Qt_Key_Key_Stop         #              # KEY_XF86Stop   ;
+'\u{F735}'	# Menu        # Qt_Key_Key_Menu         # ContextMenu  # KEY_Menu       ;
 //'\u{F736}'	# User        #                         #              ;
 //'\u{F737}'	# System      #                         #              ;
 //'\u{F738}'	# Print       # Qt_Key_Key_Print        #              ;
